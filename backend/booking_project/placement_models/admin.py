c5748c0a51e56c5a130f17857c110c5615125176
Ternario <andrii.shevchenko.6.9.6@gmail.com>
2024-10-29 23:49:01 +0100

    init

---
+++ backend/booking_project/placement_models/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from booking_project.placement_models.models.apartments import Apartments
+from booking_project.placement_models.models.categories import Categories
+
+
+# Register your models here.
+
+
+class ApartmentsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'owner', 'created_at', 'is_active')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'description')
+    list_editable = ('is_active',)
+    list_filter = ('is_active', 'created_at')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+admin.site.register(Categories)
+admin.site.register(Apartments, ApartmentsAdmin)
